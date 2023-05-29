f1f359ff67c1bda6884b32d5d8cead46337fa90d
ridwanullah-sys <abdraufridwantemi@gmail.com>
2023-05-29 21:33:17 +0100

    new commit

--- config-overrides.js
+++ config-overrides.js
@@ -11,6 +11,7 @@ module.exports = function override(config) {
     os: require.resolve("os-browserify"),
     url: require.resolve("url"),
     net: false,
+    zlib: false,
   });
   config.resolve.fallback = fallback;
   config.plugins = (config.plugins || []).concat([
