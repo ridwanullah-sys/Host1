7eeb68f11743c8449064f7d71a1d3792ded4a616
ridwanullah-sys <abdraufridwantemi@gmail.com>
2023-05-30 09:26:07 +0100

    test

--- config-overrides.js
+++ config-overrides.js
@@ -10,8 +10,18 @@ module.exports = function override(config) {
     https: require.resolve("https-browserify"),
     os: require.resolve("os-browserify"),
     url: require.resolve("url"),
-    net: false,
-    zlib: false,
+    child_process: "empty",
+    fs: require.resolve("browserify-fs"),
+    util: require.resolve("util"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    tls: require.resolve("tls-browserify"),
+    net: require.resolve("net-browserify"),
+    crypto: require.resolve("crypto-browserify"),
+    path: require.resolve("path-browserify"),
+    os: require.resolve("os-browserify"),
+    stream: require.resolve("stream-browserify"),
+    zlib: require.resolve("browserify-zlib"),
   });
   config.resolve.fallback = fallback;
   config.plugins = (config.plugins || []).concat([
